85a967209752afe02cf37cdf2abebab2c2736003
Lina Adkins <6924569+SwapBap@users.noreply.github.com>
2019-05-24 10:15:23 -0400

    Added some tooltips to config

--- Assets/CastleDBImporter/Scripts/CastleDBConfig.cs
+++ Assets/CastleDBImporter/Scripts/CastleDBConfig.cs
@@ -5,10 +5,18 @@ using UnityEngine;
  {
     public class CastleDBConfig : ScriptableObject
     {
+        [Tooltip("The CastleDB column name that is used to generate variable names/types. If this has spaces they will be replaced with '_'s") ]
         public string GUIDColumnName;
+
+        [Tooltip("The folder that the generated types will be located in.")]
         public string GeneratedTypesLocation;
+
+        [Tooltip("The namespace that the generated types will be wrappe din.")]
         public string GeneratedTypesNamespace;
+
+        [Tooltip("Suppresses import/generation information in the console.")]
         public bool SuppressBuildInfo;
+
         public static bool NamesHaveSpaces = false;
 
         public static CastleDBConfig Instance()
